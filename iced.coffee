9ab358658588556ccf9cd2bf68a202a081839c3a
danx0r <danbmil99@gmail.com>
2012-01-28 18:14:27 -0800

    clarify order of operation

--- iced.coffee
+++ iced.coffee
@@ -1,8 +1,10 @@
 dub = (n, cb) ->
-    cb n*2
+    setTimeout (-> cb n*2), 2000
 
 main = ->
     await dub 10, defer v
     console.log "result (in callback) of dub(10):", v
+    console.log "this runs last"
 
 main()
+console.log "this runs first"
--- testjs.js
+++ testjs.js
@@ -1,11 +1,15 @@
 dub = function (n, cb) {
-    cb(n*2)
+    setTimeout(function () {
+        cb(n*2)        
+    }, 2000)
 }
 
 main = function(){
     dub(10, function(v){
         console.log("result (in callback) of dub(10):", v);
+        console.log ("this runs last");
     });
 }
 
 main()
+console.log ("this runs first");
