d8d1695669f2c30abed8e91a9b97eb0d2dd31089
danx0r <danbmil99@gmail.com>
2012-01-29 00:44:40 -0800

    arrrgh, replace tabs with spaces

--- test.js
+++ test.js
@@ -1,17 +1,17 @@
 (function() {
-	var dub, main;
-	dub = function(n, cb) {
-		return setTimeout((function() {
-			return cb(n * 2);
-		}), 1000);
-	};
-	main = function() {
-		return dub(10, function(v) {
-			return dub(v, function(v2) {
-				return console.log("result (in callback) of dub(10) ** 2:", v2);
-			});
-		});
-	};
-	main();
-	console.log("this runs first");
+    var dub, main;
+    dub = function(n, cb) {
+        return setTimeout((function() {
+            return cb(n * 2);
+        }), 1000);
+    };
+    main = function() {
+        return dub(10, function(v) {
+            return dub(v, function(v2) {
+                return console.log("result (in callback) of dub(10) ** 2:", v2);
+            });
+        });
+    };
+    main();
+    console.log("this runs first");
 }).call(this);
